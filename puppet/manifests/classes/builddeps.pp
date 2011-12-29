9d9ff464daffbabe8ea3195c0d2ecac5baf6602c
Josh Matthews <josh@joshmatthews.net>
2011-12-29 17:52:02 -0500

    Working build!

--- puppet/manifests/classes/builddeps.pp
+++ puppet/manifests/classes/builddeps.pp
@@ -1,6 +1,6 @@
 class builddeps {
       package {
-          [ "libasound2-dev", "libcurl4-openssl-dev", "libnotify-dev", "libxt-dev", "libiw-dev", "mesa-common-dev", "autoconf2.13", "yasm" ]:
+          [ "libasound2-dev", "libcurl4-openssl-dev", "libnotify-dev", "libxt-dev", "libiw-dev", "mesa-common-dev", "autoconf2.13", "yasm", "binutils-gold", "x11-xserver-utils", "xserver-xorg", "xserver-xorg-core", "fluxbox", "xinit", "xterm", "dkms" ]:
               ensure => installed;
       }
 
--- puppet/manifests/classes/source.pp
+++ puppet/manifests/classes/source.pp
@@ -1,10 +1,16 @@
 class source {
     exec { "clone-mc":
-        command => "/usr/bin/hg clone http://hg.mozilla.org/mozilla-central /home/vagrant//mozilla-central",
+        command => "/usr/bin/hg clone http://hg.mozilla.org/mozilla-central /home/vagrant/mozilla-central",
         require => [Package['mercurial']],
         creates => "/home/vagrant/mozilla-central/LICENSE",
         user => "vagrant",
         timeout => 0,
         logoutput => true;
+    } ->
+
+    exec { "mozconfig":
+        command => "/bin/echo 'ac_add_options --disable-debug-symbols; export LDFLAGS=\"-Wl,--no-keep-memory\"; ac_add_options --enable-application=browser; mk_add_options MOZ_MAKE_FLAGS=\"-j2\"' >/home/vagrant/mozilla-central/.mozconfig",
+        creates => "/home/vagrant/mozilla-central/.mozconfig",
+        user => "vagrant"
     }
 }
